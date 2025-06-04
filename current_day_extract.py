267cf4d1440ebebdd9f4477d131dd27bcd63b69f
Jan Derick Velez <jderickvelez@gmail.com>
2025-06-04 16:25:46 +0800

    dockerized database and working current_day_extract.py

--- current_day_extract.py
+++ current_day_extract.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from sqlalchemy.orm import Session
 from databasemanager import DrawResults, WinningCombinations, engine
 
-file_name = rf"D:\development\python\PCSO_LOTTO_NUMBERS\html\pcso_{datetime.strftime(datetime.now(),'%Y%m%d')}.html"
+file_name = rf"D:\development\python\PCSO_LOTTO_NUMBERS\data\html\pcso_{datetime.strftime(datetime.now(),'%Y%m%d')}.html"
 if not os.path.exists(file_name):
  
     url = "https://www.pcso.gov.ph"
@@ -27,6 +27,12 @@ data = Path(file_name).read_text()
 
 soup = BeautifulSoup(data, 'html.parser')
 results = soup.find_all('div', class_='draw-game') 
+
+
+print(results)
+
+
+
 lotto_results = [] 
 for result in results:
     numbers = result.find_all_next('span', class_='draw-number',limit = 6)
@@ -39,22 +45,23 @@ for result in results:
     for number in numbers:
         winning_numbers.append(int(number.text))
     
+    
     # formatted
     amount = decimal.Decimal(jackpot_amount[1:].replace(",",""))
     winners = int(number_of_winners.replace(" Jackpot Winner/s",""))
     date = datetime.strptime(draw_date, '%B %d, %Y').date()
 
-    
+        
     with Session(engine) as session:
         lotto_result = DrawResults(raw_lotto_game=game_type, raw_jackpot=amount, raw_draw_date=date, raw_winners=winners)
-        existing_result = session.query(DrawResults).filter_by(game=lotto_result.raw_lotto_game,
-                                                                draw_date=lotto_result.raw_draw_date).one_or_none()
+        existing_result = session.query(DrawResults).filter_by(raw_lotto_game=lotto_result.raw_lotto_game,
+                                                                raw_draw_date=lotto_result.raw_draw_date).one_or_none()
         if existing_result is None:
             session.add(lotto_result)
             session.flush()
             session.refresh(lotto_result)
             for number in numbers:
-                winning_number = WinningCombinations(lotto_id=draw_results.id, draw_number=int(number.text))
+                winning_number = WinningCombinations(lotto_id= lotto_result.id, draw_number=int(number.text))
                 session.add(winning_number)
         session.commit()
         
